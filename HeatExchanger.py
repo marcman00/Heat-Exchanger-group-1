eeed6fd61c12c38ef61f3c1d81c5ca988af0e216
marcman00 <mreynolds746@gmail.com>
2016-05-17 11:23:19 -0600

    Add files via upload

---
+++ HeatExchanger.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 09:28:12 2016
+
+@author: Marcus
+"""
+def heatExchanger():
+# run all functions
+
+### Pseduocode shell
+def getInput():
+    
+def isValid(input):
+    
+def convertUnits(input):
+    
+def solveArea(input):
+    
+def solveT(input):
+    
+def solveCost(input):
+    
+def output(SolveArea(),SolveT(), solveCost()):
+    
