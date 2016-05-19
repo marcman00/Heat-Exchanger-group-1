f644896f6aec9e9af7c00cbc90f6acf0b93da872
marcman00 <mreynolds746@gmail.com>
2016-05-19 02:13:28 -0600

    Update HeatExchanger.py

--- HeatExchanger.py
+++ HeatExchanger.py
@@ -2,23 +2,38 @@
 """
 Created on Tue May 17 09:28:12 2016
 
-@author: Marcus
+@author: Group 1
+Project link here: http://apmonitor.com/che263/uploads/Main/heat_exchanger_project.pdf
 """
 def heatExchanger():
-# run all functions here
+# Source functions, runs all the other functions
 
 ### Pseduocode shell
 def getInput():
+    # All variables are set here.
+    # Note that cp is the average of inlet and outlet temperatures
+"""user can specify:
+        aes or si units
+        U, Tci, Thi, mc, mh, Tco or Tho
+        Cold: water or 1,1,1,2-Tetrafluoroethane or ethanol or 2,2,4-trimethylpentane
+        Hot: same options as cold
+"""
+
+def isValid(value):
+    # checks if a number is negative. If so, returns error message and exits program.    
     
-def isValid(input):
+def convertUnits(value):
+    # convert AES to SI units
     
-def convertUnits(input):
+def solveArea():
+    # Calculates area from input
     
-def solveArea(input):
+def solveT():
+    # calculates Tco or Tho (whichever one was NOT given in getInput() )
     
-def solveT(input):
+def solveCost():
+    # cost = $1000 * area (m^2)
     
-def solveCost(input):
-    
-def output(SolveArea(),SolveT(), solveCost()):
+def output(area, T, cost):
+    # Displays solutions all in SI units
     
