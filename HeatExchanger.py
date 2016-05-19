0c4caa8fb6850ad13af17200e696084eb0bfd3a8
marcman00 <mreynolds746@gmail.com>
2016-05-19 10:55:52 -0600

    Update HeatExchanger.py

--- HeatExchanger.py
+++ HeatExchanger.py
@@ -5,35 +5,37 @@ Created on Tue May 17 09:28:12 2016
 @author: Group 1
 Project link here: http://apmonitor.com/che263/uploads/Main/heat_exchanger_project.pdf
 """
-def heatExchanger():
-# Source functions, runs all the other functions
-
-### Pseduocode shell
-def getInput():
-    # All variables are set here.
-    # Note that cp is the average of inlet and outlet temperatures
-"""user can specify:
-        aes or si units
-        U, Tci, Thi, mc, mh, Tco or Tho
-        Cold: water or 1,1,1,2-Tetrafluoroethane or ethanol or 2,2,4-trimethylpentane
-        Hot: same options as cold
-"""
-
-def isValid(value):
-    # checks if a number is negative. If so, returns error message and exits program.    
+class HeatExchanger():
     
-def convertUnits(value):
-    # convert AES to SI units
+    def runAll():
+    # Source functions, runs all the other functions
     
-def solveArea():
-    # Calculates area from input
+    ### Pseduocode shell
+    def getInput():
+        # All variables are set here.
+        # Note that cp is the average of inlet and outlet temperatures
+    """user can specify:
+            aes or si units
+            U, Tci, Thi, mc, mh, Tco or Tho
+            Cold: water or 1,1,1,2-Tetrafluoroethane or ethanol or 2,2,4-trimethylpentane
+            Hot: same options as cold
+    """
     
-def solveT():
-    # calculates Tco or Tho (whichever one was NOT given in getInput() )
-    
-def solveCost():
-    # cost = $1000 * area (m^2)
-    
-def output(area, T, cost):
-    # Displays solutions all in SI units
-    
+    def isValid(value):
+        # checks if a number is negative. If so, returns error message and exits program.    
+        
+    def convertUnits(value):
+        # convert AES to SI units
+        
+    def solveArea():
+        # Calculates area from input
+        
+    def solveT():
+        # calculates Tco or Tho (whichever one was NOT given in getInput() )
+        
+    def solveCost():
+        # cost = $1000 * area (m^2)
+        
+    def output(area, T, cost):
+        # Displays solutions all in SI units
+        
