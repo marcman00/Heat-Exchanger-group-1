aa6949a83e01e9999fb63b026e7fa0deafb64d98
marcman00 <mreynolds746@gmail.com>
2016-05-19 12:03:57 -0600

    Update HeatExchanger.py

--- HeatExchanger.py
+++ HeatExchanger.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 17 09:28:12 2016
-Due on 
-@author: Group 1
+Due on Tue May 31 9:00am
+@author: Group 1-- Marcus Reynolds, Gabriel Poulson, Dan Addington
 Project link here: http://apmonitor.com/che263/uploads/Main/heat_exchanger_project.pdf
 """
 class HeatExchanger():
     # Variables that are given in getInput() and then used in another function are declared here
     units=""    # aes or si
     U=0         # Heat Transfer Coefficient
-    Tci=0       # T cold fluid, in
-    Thi=0       # T hot fluid, in
-    mc=0        # flow rate cold fluid
-    mh=0        # flow rate hot fluid
+    T_ci=0       # T cold fluid, in
+    T_hi=0       # T hot fluid, in
+    m_c=0        # flow rate cold fluid
+    m_h=0        # flow rate hot fluid
     cold=""     # options given below in input
     hot=""      # options given below in input
-    given=""    # Tco or Tho
+    given=""    # T_co or T_ho
     Cp=0        # Heat capacity
     
     def __init__(self):
@@ -34,11 +33,10 @@ class HeatExchanger():
    
 # Gabriel
     def getInput():
-        # All variables are set here.
-        # Note that cp is the average of inlet and outlet temperatures
+        # Variables are set here.
     """user can specify:
             aes or si units
-            U, Tci, Thi, mc, mh, Tco or Tho
+            U, T_ci, T_hi, m_c, m_h, T_co or T_ho
             Cold: water or 1,1,1,2-Tetrafluoroethane or ethanol or 2,2,4-trimethylpentane
             Hot: same options as cold
     """
@@ -50,27 +48,33 @@ class HeatExchanger():
         # convert AES to SI units
 # Dan        
     def solveArea():
-        # Calculates area from input
+    # Calculates area from input
+    
+        return area
         
     def solveT():
-        # calculates Tco or Tho (whichever one was NOT given in getInput() )
+    # calculates Tco or Tho (whichever one was NOT given in getInput() )
         # (Hint: You will need to determine the unspecified temperature from the first two listed equations.)
-        if given=="Tco":
-            cp=(Tco+Tci)/2
+        # Cp is the average of inlet and outlet temperatures
+        if given=="T_co":
+            Cp=(T_co+T_ci)/2
             # something
         else:
-            cp=(Tho+thi)/2
+            Cp=(T_ho+T_hi)/2
             # something else
+        return T
         
     def solveCost():
         # cost = $1000 * area (m^2)
+        
+        return cost
         
 # Marcus        
     def output(area, T, cost):
         # Displays solutions all in SI units
         print("Surface Area=",area,"$m^2$")
-        if given=="Tco":
-            print("$T_ho$=",T,"K")
+        if given=="T_co":
+            print("T_ho=",T,"K")
         else:
-            print("$T_co$=",T,"K")
+            print("T_co=",T,"K")
         print("Cost= $"+str(cost))
